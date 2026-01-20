b1d1ff9c2b1b7d8d6baf325abc9508ee540256dc
Habtamu <habtuwendifraw@gmail.com>
2026-01-20 12:57:22 +0300

    Align raw loader and dbt staging with analytics-ready Telegram schema

--- medical_warehouse/models/staging/stg_telegram_messages.sql
+++ medical_warehouse/models/staging/stg_telegram_messages.sql
@@ -1,23 +1,17 @@
--- models/staging/stg_telegram_messages.sql
-
-with raw_data as (
+with source as (
 
     select
         message_id,
-        channel_name,
-        message_date::timestamp        as message_date,
+        message_date::timestamp as message_date,
+        sender_id,
         message_text,
-        length(message_text)           as message_length,
-        null::integer                  as view_count,
-        null::integer                  as forward_count,
-        case
-            when image_path is not null then true
-            else false
-        end                            as has_image,
+        channel_name,
+        views,
+        forwards,
+        has_media,
         image_path
-    from {{ source('raw', 'raw_telegram_data') }}
+    from raw_telegram_data
 
 )
 
-select *
-from raw_data
+select * from source
